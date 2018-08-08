b1faaebe07d7ae61a2ff123b6d82d0f90e782705
Raul Rodriguez <Raul@Maxs-MacBook-Pro.local>
2018-08-08 10:42:30 -0700

    Some basic app functionality

---
+++ artist_service.py
@@ -0,0 +1,22 @@
+artist_info = {
+    'Drake': {
+    'image': 'Drake.jpg',
+    'spotify_link': '3TVXtAsR1Inumwj472S9r4',
+},
+    'Kendrick Lamar': {
+    'image': 'Kendrick Lamar.jpg',
+    'spotify_link': '2YZyLoL8N0Wb9xBt1NhZWg',
+},
+    'Travis Scott': {
+    'image': 'Travis Scott.jpg',
+    'spotify_link': '0Y5tJX1MQlPlqiwlOH1tJY'
+},
+    'Kid Cudi': {
+    'image': 'Kid Cudi.jpg',
+    'spotify_link': '0fA0VVWsXO9YnASrzqfmYu',
+},
+    'Khalid': {
+    'image': 'Khalid.jpg',
+    'spotify_link': '6LuN9FCkKOj5PcnpouEgny',
+},
+}
